c5ec8ff64473a23c55137109e32b996e704df9c8
Anisur Rahman <anisurlist3@gmail.com>
2018-01-09 11:47:58 +0600

    rearange something

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
 require __DIR__ . DIRECTORY_SEPARATOR . 'inc/autoload.php';
 require __DIR__ . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-
-use Anis\Post;
-use Anis\Config;
-
-$objPost = new Post();
-$posts = $objPost->paginate(2);
-
-// // var_dump($post);
-// // $objPost->insert([
-// //     'name' => 'anis',
-// //     'email' => 'anisu@yahoo.com',
-// //     'fullname' => 'Anisur Rahman'
-// // ]);
-foreach ($posts as $post) {
-    echo '<li>' . $post->title . '</li>';
-}
-echo $objPost->links(1, 'anis');
-
-// var_dump($posts);
-// echo ROOT_PATH;
---
+++ src/validation/Validator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Anis\Validation;
+
+class Validator
+{
+    public function __construct()
+    {
+        return;
+    }
+}
